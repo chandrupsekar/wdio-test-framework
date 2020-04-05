2618074c65a38aa8d4da7306badb88e1f5166a14
Rahul Rawat <rawat1416@gmail.com>
2020-04-05 17:29:14 +0530

    database tests

--- src/test/specs/database.test.js
+++ src/test/specs/database.test.js
@@ -1,22 +1,13 @@
 const mysql = require('mysql')
-const util = require('../')
+const db = require('../../../util/database.util')
+
+
 describe('Database Connectivity', ()=>{
-    it('fetching data from database', ()=>{
-        
-        
-        var article={
-            author : 'Shree Ramchandra',
-            title : 'Node tutorial',
-            body: 'foo bar'
-        }
-        
-        var query= connection.query('select * from articles', function(err, result){
-            if(err){
-                console.log(err)
-                return
-            }
-            console.log(result);
-        })
-        connection.end()
+    it('fetching from the database', ()=>{
+        db.retrieve()
+    })
+
+    it('inserting into the database', ()=>{
+        db.insert()
     })
 })
