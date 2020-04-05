5be8940111b3ecd84b8a7408961401b79ea86899
Rahul Rawat <rawat1416@gmail.com>
2020-04-05 17:19:14 +0530

    database tests added

--- src/test/specs/database.test.js
+++ src/test/specs/database.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const dbase = require('../../../util/database.util')
 
 //before hook is used before every describe block
@@ -11,28 +10,3 @@ describe('Database Connectivity', ()=>{
         dbase.insert()
     })
 })
-||||||| merged common ancestors
-=======
-const mysql = require('mysql')
-const util = require('../')
-describe('Database Connectivity', ()=>{
-    it('fetching data from database', ()=>{
-        
-        
-        var article={
-            author : 'Shree Ramchandra',
-            title : 'Node tutorial',
-            body: 'foo bar'
-        }
-        
-        var query= connection.query('select * from articles', function(err, result){
-            if(err){
-                console.log(err)
-                return
-            }
-            console.log(result);
-        })
-        connection.end()
-    })
-})
->>>>>>> 53c2bc9369a825f0a154d35638d2ad8be73faad0
