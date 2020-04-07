3fe348a45dc16b60cbc48af400302d425997e923
Rahul Rawat <rawat1416@gmail.com>
2020-04-07 15:55:56 +0530

    updating with required changes

--- wdio.conf.js
+++ wdio.conf.js
@@ -34,7 +34,7 @@ exports.config = {
     //
     // First, you can define how many instances should be started at the same time. Let's
     // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
+    // set Instances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
     // files and you set maxInstances to 10, all spec files will get tested at the same time
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
@@ -54,7 +54,7 @@ exports.config = {
     //     //build: process.env.BUILD_NUMBER
     },{
         browserName: 'chrome',
-        browserVersion: 'latest',
+        //browserVersion: 'latest',
         //maxInstances: 2
         //name: 'verify login page title',
         //build: process.env.BUILD_NUMBER
