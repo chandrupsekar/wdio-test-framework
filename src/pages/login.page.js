d286968ffdb4e46fc819a3f6ebb845d36eb66a42
Nagaraj M <nagarajbumca@outlook.com>
2020-04-22 12:16:05 +0530

    merge conflicts and cleanup

--- src/pages/login.page.js
+++ src/pages/login.page.js
@@ -15,18 +15,9 @@ class LoginPage {
     
     doLogin(emailID, pwd) {
         elementUtil.doSetValue(loginPageElements.username, emailID)
-<<<<<<< HEAD
-        elementUtil.doSetValue(loginPageElements.password,  pwd)
-        elementUtil.doClick(loginPageElements.loginBtn)
-||||||| merged common ancestors
-        elementUtil.doSetValue(loginPageElements.password, pwd)
-        elementUtil.doClick(loginPageElements.loginBtn)
-=======
         elementUtil.doSetValue(loginPageElements.password, pwd)
         elementUtil.clickElement(loginPageElements.loginBtn)
->>>>>>> 3b6c3be88d293f03c113a7f07b48d6e4ab3ce9a2
     }
-<<<<<<< HEAD
     clickOnCheckBox(element){
         if(!isCheckboxSelected)
             elementUtil.doClick(loginPageElements.clickableCheckBoxElement)
@@ -34,12 +25,9 @@ class LoginPage {
     isCheckboxSelected(element){
         return element.isElementSelected()
     }
-||||||| merged common ancestors
-=======
     clickOnCheckBox(){
         browser.pause(5000)
         elementUtil.clickElement(loginPageElements.clickableCheckBoxElement)
     }  
->>>>>>> 3b6c3be88d293f03c113a7f07b48d6e4ab3ce9a2
 }
 module.exports = new LoginPage()
